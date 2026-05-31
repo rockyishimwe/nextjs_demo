d04dd9b641a2f0d6d7815dff91181ee38bab72eb
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-31 12:47:29 +0200

    going forward with dynamic routing

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,15 @@
 const users = () => {
   return (
-    <div>users</div>
-  )
-}
+    <div>
+      <h1>Dashboard Users</h1>
+      <ul className="mt-10">
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+        <li>User 4</li>
+      </ul>
+    </div>
+  );
+};
 
-export default users
+export default users;
