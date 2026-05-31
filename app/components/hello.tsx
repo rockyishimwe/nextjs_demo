57a61d08e04b35cff826c1700a929bfc07fe1e44
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-31 12:27:24 +0200

    enabling react compiler

---
+++ app/components/hello.tsx
@@ -0,0 +1,9 @@
+'use client';
+const hello = () => {
+    console.log("i am a client component");
+  return (
+    <div>hello</div>
+  )
+}
+
+export default hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Hello from "@/app/components/hello";
 const page = () => {
+
+  console.log("what type of a component am I ?");
   return (
-    <div>Welcome to Next.js!</div>
+    <main>
+      <div className="text-5xl underline">Welcome to Next.js!</div>
+      <Hello/>
+    </main>
   )
 }
 
