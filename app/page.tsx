0459e335bd7e2a78813f3e06d4f57392996b6758
rocky ishimwe <rockyishimwe9@gmail.com>
2026-06-03 13:56:45 +0200

    :added black background

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import ExploreBtn from "@/components/ExploreBtn"
+
 const page = () => {
   return (
     <section>
       <h1 className="text-center">The Hub For Every Dev<br/>Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons , Meetups, and Conferences, All in One Place</p>
+      <ExploreBtn />
     </section>
     
   )
