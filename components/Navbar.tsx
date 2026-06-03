4f1ebad0140df3247b813b9a8f507ab83e42b0e3
rocky ishimwe <rockyishimwe9@gmail.com>
2026-06-03 14:04:00 +0200

    :started implementation

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <Link href="/" className="logo">
+                </Link>
+            </nav>
+        </header>
+    )
+}
