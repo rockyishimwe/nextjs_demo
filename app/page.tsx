84bff172681f5ac34468d3d65279d7ce3698aff4
rocky ishimwe <rockyishimwe9@gmail.com>
2026-06-04 06:57:38 +0200

    :added a nvabar component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,14 @@ const page = () => {
       <h1 className="text-center">The Hub For Every Dev<br/>Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons , Meetups, and Conferences, All in One Place</p>
       <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured 
+          Events
+          <ul className="events">{[1,2,3,4,5].map((event)=>(
+            <li key={event}>Event {event}</li>
+          ))}</ul>
+        </h3>
+      </div>
     </section>
     
   )
--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -5,7 +5,7 @@ const ExploreBtn = () => {
     <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
       <a href="#events">
         Explore Events
-        <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
+        <Image src="/icons/arrow-down.svg" alt="arrow-down" width={20} height={20} />
       </a>
     </button>
   )
