51fa14faaeb04825afa2aa0d3f21c1b38625a8e0
rocky ishimwe <rockyishimwe9@gmail.com>
2026-06-04 19:57:43 +0200

    :started adding constants to use on the page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,19 @@
-export 
+export type EventItem  = {
+    image:string;
+    title:string;
+    slug:string;
+    location:string;
+    date:string;
+    time:string;
+};
+export const events:EventItem[] = [
+    {
+        image:"/images/event1.png",
+        title:"React Summit US 2025",
+        slug:"react-summit-us-2025",
+        location:"San Francisco, CA, USA",
+        date:"2025-11-07",
+        time:"09:00 AM",
+    },
+    
+]
