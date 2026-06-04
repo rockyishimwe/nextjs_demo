091b924f6d14cc6bdd50bd10b49045c0494ced18
rocky ishimwe <rockyishimwe9@gmail.com>
2026-06-04 15:08:54 +0200

    :finalized the eventcard section

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
+import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 
 const page = () => {
   return (
     <section>
-      <h1 className="text-center">The Hub For Every Dev<br/>Event You Can't Miss</h1>
-      <p className="text-center mt-5">Hackathons , Meetups, and Conferences, All in One Place</p>
-      <ExploreBtn />
-      <div className="mt-20 space-y-7">
-        <h3>Featured 
-          Events
-          <ul className="events">{[1,2,3,4,5].map((event)=>(
-            <li key={event}>Event {event}</li>
-          ))}</ul>
-        </h3>
-      </div>
-    </section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+            <ExploreBtn />
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
     
   )
 }
