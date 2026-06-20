350c47c70b072d0fd2850278a3f60cefceec465a
rocky ishimwe <rockyishimwe9@gmail.com>
2026-06-20 14:42:08 +0200

    refining the booking.model.ts'

---
+++ app/database/booking.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model, models, Document, Types } from 'mongoose';
+import Event from './event.model';
+
+// TypeScript interface for Booking document
+export interface IBooking extends Document {
+  eventId: Types.ObjectId;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+const BookingSchema = new Schema<IBooking>(
+  {
+    eventId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Event',
+      required: [true, 'Event ID is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      trim: true,
+      lowercase: true,
+       validate: {
+        validator: function (email: string) {
+          // RFC 5322 compliant email validation regex
+          const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+          return emailRegex.test(email);
+        },
+        message: 'Please provide a valid email address',
+      },
+    },
+  },
+  {
+    timestamps: true, // Auto-generate createdAt and updatedAt
+})
