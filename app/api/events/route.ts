586e52ecfe94a4864550e0e72baee3a2ec171f63
rocky ishimwe <rockyishimwe9@gmail.com>
2026-06-30 18:00:46 +0200

    :added a catch block for error handling

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -57,5 +57,7 @@ export async function POST(req: NextRequest) {
 export async function GET(){
   try {
     await connectDB();
+  } catch(e){
+    return NextResponse.json({message:'Event fetching failed',error:e},{status:500});
   }
 }
