49ca048da6f29cfe812949d9fa68c0e6df1e3aef
rocky ishimwe <rockyishimwe9@gmail.com>
2026-07-02 09:45:15 +0200

    :added a return response to know that the events were fetched successfully

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -58,6 +58,7 @@ export async function GET(){
   try {
     await connectDB();
     const events = await Event.find().sort({created:-1});
+    return NextResponse.json({message:'Events fetched successfully',events},{status:200});
   } catch(e){
     return NextResponse.json({message:'Event fetching failed',error:e},{status:500});
   }
