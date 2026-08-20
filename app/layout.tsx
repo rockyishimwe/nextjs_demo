e658ac428e1de7e271fb61ed6055291547846af8
rocky ishimwe <rockyishimwe9@gmail.com>
2026-08-20 19:56:44 +0200

    fixing layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,14 @@
 import type { Metadata } from "next";
-import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Suspense } from "react";
 import { Toaster } from "sonner";
-
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 import ConditionalLightRays from "@/app/_components/ConditionalLightRays";
 import Navbar from "@/components/Navbar";
 import { PostHogProvider } from "./providers";
 import PostHogPageView from "./_components/PostHogPageView";
 import { ClerkProvider } from "@clerk/nextjs";
-
-const schibstedGrotesk = Schibsted_Grotesk({
-  variable: "--font-schibsted-grotesk",
-  subsets: ["latin"],
-});
-
-const martianMono = Martian_Mono({
-  variable: "--font-martian-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "DevEvent",
@@ -39,9 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans")}>
       <body
-        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+        className={`${cn(
+          "font-schibsted-grotesk",
+          "font-martian-mono"
+        )} min-h-screen antialiased`}
       >
         <ClerkProvider>
           <PostHogProvider>
